188c0dd6c7c2f92df83b437eaf5bd6525bef516f
REXWindW <1114864532@qq.com>
2024-03-12 12:16:20 +0800

    修改了一下正则

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from callgpt import API_KEYS
-from callgpt import call_gpt
+from utils.callgpt import API_KEYS
+from utils.callgpt import call_gpt
 import random
 import json
+import re
 
 now_role = '''你是一个专业的医疗助手'''
 
-prompt_list = '''以上是一段医学相关的文本资料，请你根据上述资料的内容，给出针对文本中知识的一问一答，提问和回答请用换行分开，不要有多余输出'''
+prompt_list = '''以上是一段医学相关的文本资料，请你根据上述资料的内容，给出针对文本中知识的三对问答，提问和回答请用换行分开，不要有多余输出'''
+
+def split_response(original):
+    questions = original.split('\n')
+    result = []
+    for question in questions:
+        question = re.sub(r':\s*', '', question) # 匹配冒号后的内容
+        if len(question) > 5:# 要大于一定长度才保留，这里主要是为了防止多余空行
+            result.append(question)
+    return result
 
 def process(qa_list,prompt): # 处理单条对话
     prompt = prompt + '\n' + random.choice(prompt_list.split('\n'))
@@ -14,9 +24,12 @@ def process(qa_list,prompt): # 处理单条对话
     # 获得随机prompt和keys
     rt = call_gpt(now_role, prompt, key)
     # 再根据问答处理以下
-    tmp = rt.split('\n')
+    tmp = split_response(rt)
     print(tmp)
-    qa_list[tmp[0]] = tmp[1] # 写入问答
+    # 写入这三组问答
+    qa_list[tmp[0]] = tmp[1]
+    qa_list[tmp[2]] = tmp[3]
+    qa_list[tmp[4]] = tmp[5]
 
 def work():
     # 先读入input.txt
